9fb6230136b726a2e8bf30e13cd506efaa50dac1
Ramkumar <ram.9107@gmail.com>
2023-12-10 05:45:51 +0530

    adding go routines and channels

--- main.go
+++ main.go
@@ -1,79 +1,141 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"os"
+	"strconv"
+	"time"
+
 	"github.com/shirou/gopsutil/v3/process"
+	// other necessary imports
 )
 
-func main() {
-	// Get a list of all running processes
-	pids, err := process.Pids()
+// DataStruct represents the structure of the data to be collected.
+type DataStruct struct {
+	// define your fields here
+	Pid       int32
+	Name      string
+	Cpu       float64
+	Rss       uint64
+	Vms       uint64
+	ReadIO    uint64
+	WriteIO   uint64
+	Timestamp time.Time
+}
+
+func collectData(dataChan chan<- DataStruct) {
+
+	for {
+		pids, err := process.Pids()
+		if err != nil {
+			panic(err)
+		}
+
+		for _, pid := range pids {
+			// Skip pid 0 as it is reserved and won't have corresponding process details
+			if pid == 0 {
+				continue
+			}
+
+			// Get process details
+			p, err := process.NewProcess(pid)
+			if err != nil {
+				fmt.Printf("Error while fetching process %d: %s\n", pid, err)
+				continue
+			}
+
+			// Get process name
+			name, err := p.Name()
+			if err != nil {
+				fmt.Printf("Error while fetching name for process %d: %s\n", pid, err)
+				continue
+			}
+
+			// Get CPU percent
+			cpuPercent, err := p.CPUPercent()
+			if err != nil {
+				fmt.Printf("Error while fetching CPU percent for process %d: %s\n", pid, err)
+				continue
+			}
+
+			// Get memory info
+			memInfo, err := p.MemoryInfo()
+			if err != nil {
+				fmt.Printf("Error while fetching memory info for process %d: %s\n", pid, err)
+				continue
+			}
+
+			// Get IO counters
+			ioCounters, err := p.IOCounters()
+			if err != nil {
+				fmt.Printf("Error while fetching IO counters for process %d: %s\n", pid, err)
+				continue
+			}
+
+			fmt.Println("---------------------------------------------------------")
+			data := DataStruct{
+				Pid:       pid,
+				Name:      name,
+				Cpu:       cpuPercent,
+				Rss:       memInfo.RSS,
+				Vms:       memInfo.VMS,
+				ReadIO:    ioCounters.ReadCount,
+				WriteIO:   ioCounters.WriteCount,
+				Timestamp: time.Now(), // Current time
+			}
+
+			// Send the data to the channel
+			dataChan <- data
+
+		}
+		// Collect data and create a DataStruct instance
+
+		// Wait for 10 seconds before the next collection
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func writeToCSV(dataChan <-chan DataStruct) {
+	// Open or create the CSV file
+	file, err := os.OpenFile("data.csv", os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
+	defer file.Close()
 
-	for _, pid := range pids {
-		// Skip pid 0 as it is reserved and won't have corresponding process details
-		if pid == 0 {
-			continue
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for data := range dataChan {
+		// Convert data to a slice of strings (or any format suitable for CSV)
+		record := []string{
+			strconv.FormatInt(int64(data.Pid), 10),    // Convert int32 to string
+			data.Name,                                 // String can be used directly
+			strconv.FormatFloat(data.Cpu, 'f', 2, 64), // Convert float64 to string with 2 decimal precision
+			strconv.FormatUint(data.Rss, 10),          // Convert uint64 to string
+			strconv.FormatUint(data.Vms, 10),          // Convert uint64 to string
+			strconv.FormatUint(data.ReadIO, 10),       // Convert uint64 to string
+			strconv.FormatUint(data.WriteIO, 10),      // Convert uint64 to string
+			data.Timestamp.Format(time.RFC3339),       // Convert time.Time to string
 		}
-
-		// Get process details
-		p, err := process.NewProcess(pid)
-		if err != nil {
-			fmt.Printf("Error while fetching process %d: %s\n", pid, err)
-			continue
+		// Write to CSV
+		if err := writer.Write(record); err != nil {
+			panic(err)
 		}
-
-		// Get process name
-		name, err := p.Name()
-		if err != nil {
-			fmt.Printf("Error while fetching name for process %d: %s\n", pid, err)
-			continue
-		}
-
-		// Get CPU percent
-		cpuPercent, err := p.CPUPercent()
-		if err != nil {
-			fmt.Printf("Error while fetching CPU percent for process %d: %s\n", pid, err)
-			continue
-		}
-
-		// Get memory info
-		memInfo, err := p.MemoryInfo()
-		if err != nil {
-			fmt.Printf("Error while fetching memory info for process %d: %s\n", pid, err)
-			continue
-		}
-
-		// Get IO counters
-		ioCounters, err := p.IOCounters()
-		if err != nil {
-			fmt.Printf("Error while fetching IO counters for process %d: %s\n", pid, err)
-			continue
-		}
-
-		// Get network connections
-		conns, err := p.Connections()
-		if err != nil {
-			fmt.Printf("Error while fetching connections for process %d: %s\n", pid, err)
-			continue
-		}
-
-		// Print process information
-		fmt.Printf("Process ID: %d\n", pid)
-		fmt.Printf("Name: %s\n", name)
-		fmt.Printf("CPU Percent: %.2f%%\n", cpuPercent)
-		fmt.Printf("Memory: %d bytes (RSS), %d bytes (VMS)\n", memInfo.RSS, memInfo.VMS)
-		fmt.Printf("IO Counters: ReadCount: %d, WriteCount: %d\n", ioCounters.ReadCount, ioCounters.WriteCount)
-
-		// Print network information
-		for _, conn := range conns {
-			fmt.Printf("Network Connection: Local Address: %s, Remote Address: %s\n", conn.Laddr.IP, conn.Raddr.IP)
-			fmt.Printf("Ports used: Local: %d, Remote: %d\n", conn.Laddr.Port, conn.Raddr.Port)
-		}
-
-		fmt.Println("---------------------------------------------------------")
+		writer.Flush()
 	}
+}
+
+func main() {
+	dataChan := make(chan DataStruct)
+
+	// Start the data collection goroutine
+	go collectData(dataChan)
+
+	// Start the CSV writing goroutine
+	go writeToCSV(dataChan)
+
+	// Keep the main goroutine alive
+	select {}
 }
